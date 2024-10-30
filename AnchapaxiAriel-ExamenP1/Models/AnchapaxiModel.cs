73bee51fe427a2948e8dd318c6d56ebd4f3a68fb
UdlaAriel <ariel.anchapaxi@udla.edu.ec>
2024-10-30 08:27:41 -0500

    Creation of the principal models.

--- AnchapaxiAriel-ExamenP1/Models/AnchapaxiModel.cs
+++ AnchapaxiAriel-ExamenP1/Models/AnchapaxiModel.cs
@@ -1,6 +1,16 @@
-﻿namespace AnchapaxiAriel_ExamenP1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnchapaxiAriel_ExamenP1.Models
 {
     public class AnchapaxiModel
     {
-    }
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public bool IsStudent { get; set; }
+        [Required]
+        public DateOnly BirthDay { get; set; }
+    }   
 }
---
+++ AnchapaxiAriel-ExamenP1/Models/PhoneModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AnchapaxiAriel_ExamenP1.Models
+{
+    public class PhoneModel
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }    
+        public int Year { get; set; }
+        public float Price { get; set; }
+    }
+}
